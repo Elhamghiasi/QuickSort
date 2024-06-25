b1dbdaec510713dd42e285b3edcd1fd60e7826ec
Elhamghiasi <149545648+Elhamghiasi@users.noreply.github.com>
2024-06-24 20:45:41 -0700

    first commit

---
+++ QuickSort/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace QuickSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSortSequential = new System.Windows.Forms.Button();
+            this.btnSortSequentialLarge = new System.Windows.Forms.Button();
+            this.btnSortParallelLarge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSortSequential
+            // 
+            this.btnSortSequential.Location = new System.Drawing.Point(378, 74);
+            this.btnSortSequential.Name = "btnSortSequential";
+            this.btnSortSequential.Size = new System.Drawing.Size(176, 23);
+            this.btnSortSequential.TabIndex = 0;
+            this.btnSortSequential.Text = "Sort Sequential";
+            this.btnSortSequential.UseVisualStyleBackColor = true;
+            this.btnSortSequential.Click += new System.EventHandler(this.btnSortSequential_Click);
+            // 
+            // btnSortSequentialLarge
+            // 
+            this.btnSortSequentialLarge.Location = new System.Drawing.Point(378, 140);
+            this.btnSortSequentialLarge.Name = "btnSortSequentialLarge";
+            this.btnSortSequentialLarge.Size = new System.Drawing.Size(176, 23);
+            this.btnSortSequentialLarge.TabIndex = 1;
+            this.btnSortSequentialLarge.Text = "Sort Sequential Large";
+            this.btnSortSequentialLarge.UseVisualStyleBackColor = true;
+            this.btnSortSequentialLarge.Click += new System.EventHandler(this.btnSortSequentialLarge_Click);
+            // 
+            // btnSortParallelLarge
+            // 
+            this.btnSortParallelLarge.Location = new System.Drawing.Point(378, 204);
+            this.btnSortParallelLarge.Name = "btnSortParallelLarge";
+            this.btnSortParallelLarge.Size = new System.Drawing.Size(176, 23);
+            this.btnSortParallelLarge.TabIndex = 2;
+            this.btnSortParallelLarge.Text = "Sort Parallel Large";
+            this.btnSortParallelLarge.UseVisualStyleBackColor = true;
+            this.btnSortParallelLarge.Click += new System.EventHandler(this.btnSortParallelLarge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSortParallelLarge);
+            this.Controls.Add(this.btnSortSequentialLarge);
+            this.Controls.Add(this.btnSortSequential);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSortSequential;
+        private System.Windows.Forms.Button btnSortSequentialLarge;
+        private System.Windows.Forms.Button btnSortParallelLarge;
+    }
+}
+
---
+++ QuickSort/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuickSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSortSequential_Click(object sender, EventArgs e)
+        {
+            double[] data = { 3, 9, 15, 7, 8, 4, 11 };
+            QuickSortAlgorithms.QuickSort<double>(data, 0, data.Length-1);
+            string out1 = "";
+            foreach (int n in data) out1 += n.ToString() + "" + "\n";
+            MessageBox.Show(out1);
+        }
+
+        private void btnSortSequentialLarge_Click(object sender, EventArgs e)
+        {
+            int size = 10000000;
+            double[] data = new double[size];
+            InitData(data);
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            QuickSortAlgorithms.QuickSort<double>(data, 0, data.Length - 1); sw.Stop();
+
+            MessageBox.Show("Sequential: Time taken = " + sw.ElapsedMilliseconds.ToString());
+        }
+
+        void InitData(double[] data)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < data.Length; i++)
+                data[i] = rand.NextDouble() * 1000 + 5;
+        }
+
+        private void btnSortParallelLarge_Click(object sender, EventArgs e)
+        {
+            int size = 10000000; double[]
+            data = new double[size];
+            InitData(data);
+            Stopwatch sw = new Stopwatch(); sw.Start();
+            QuickSortAlgorithms.QuickSortParallel<double>(data, 0, data.Length -1); sw.Stop();
+            MessageBox.Show("Parallel:Time taken = " + sw.ElapsedMilliseconds.ToString());
+        }
+    }
+}
